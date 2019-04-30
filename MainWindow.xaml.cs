6186f55c065e6977d76ad0c18909ef77b833c0f7
Tomasz Kapusta <tomasz.kapusta@sabre.com>
2019-04-30 12:44:30 +0200

    initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows;
+using System.Runtime.InteropServices;
+using System.Windows.Input;
+
+namespace Musicbar
+{
+    public partial class MainWindow : Window
+    {
+        public const int KEYEVENTF_EXTENTEDKEY = 1;
+        public const int VK_MEDIA_NEXT_TRACK = 0xB0;
+        public const int VK_MEDIA_PLAY_PAUSE = 0xB3;
+        public const int VK_MEDIA_PREV_TRACK = 0xB1;
+
+        [DllImport("user32.dll")]
+        public static extern void keybd_event(byte virtualKey, byte scanCode, uint flags, IntPtr extraInfo);
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.ShowInTaskbar = false;
+           
+        }
+
+        private void Move_Window(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void Prev_Click(object sender, RoutedEventArgs e)
+        {
+            keybd_event(VK_MEDIA_PREV_TRACK, 0, KEYEVENTF_EXTENTEDKEY, IntPtr.Zero);
+        }
+
+        private void Play_Click(object sender, RoutedEventArgs e)
+        {
+            keybd_event(VK_MEDIA_PLAY_PAUSE, 0, KEYEVENTF_EXTENTEDKEY, IntPtr.Zero);
+        }
+
+        private void Next_Click(object sender, RoutedEventArgs e)
+        {
+            keybd_event(VK_MEDIA_NEXT_TRACK, 0, KEYEVENTF_EXTENTEDKEY, IntPtr.Zero);
+        }
+    }
+}
