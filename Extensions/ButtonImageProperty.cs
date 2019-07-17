e9443e70c0856207d94b7d4935f26368653e4818
Kapu <tomasz.kapusta@sabre.com>
2019-07-17 17:58:03 +0200

    add attached property to button, but still not working in template

---
+++ Extensions/ButtonImageProperty.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace Musicbar.Extensions
+{
+    public class ButtonImageProperty : DependencyObject
+    {
+        public static readonly DependencyProperty ImagePathProperty = DependencyProperty.RegisterAttached(
+            "ImagePath", typeof(string), typeof(ButtonImageProperty), new PropertyMetadata(""));
+
+        public static void SetImagePath(DependencyObject target, string value)
+        {
+            target.SetValue(ImagePathProperty, value);
+        }
+
+        public static string GetImagePath(DependencyObject target)
+        {
+            return (string) target.GetValue(ImagePathProperty);
+        }
+    }
+}
